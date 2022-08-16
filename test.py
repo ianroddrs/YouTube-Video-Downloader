179e0a95ef6e7dc06192a7cce97c5b3f6f80a478
Ian Rodrigues <86446951+ianroddrs@users.noreply.github.com>
2022-08-15 21:12:55 -0300

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ from pytube import YouTube
 url = input(str('Url para download: '))
 caminho = input(str('salvar em: '))
 
+#BAIXAR VÍDEO
 print('BAIXANDO VIDEO...')
 video = YouTube(url)
 video.streams.get_highest_resolution().download(output_path='C:/' + caminho)
 
+#BAIXAR MP3
 print('BAIXANDO AUDIO...')
 musica = YouTube(url)
 musica.streams.filter(only_audio=True).first().download(output_path='C:/' + caminho, filename=musica.title + '.mp3')
